46eab5705a6c2c617368a91ed62e1bfa14923cfa
Thiago Silva <thiago@metareload.com>
2006-03-05 17:07:49 +0000

    -fix: line numbering

--- trunk/protoeditor/src/dbg/debuggerdbg.cpp
+++ trunk/protoeditor/src/dbg/debuggerdbg.cpp
@@ -477,7 +477,7 @@ void DebuggerDBG::debugLog(int type, const QString& msg, int line, const QString
         case E_PARSE:
         case E_COMPILE_ERROR:
         case E_USER_ERROR:
-          manager()->debugMessage(DebuggerManager::ErrorMsg, message, filePath, line);
+          manager()->debugMessage(DebuggerManager::ErrorMsg, message, filePath, line-1);
           manager()->debugError(message);
           break;
 
